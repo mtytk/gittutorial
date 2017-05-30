076097682e31846033516c3ec13d697782babf6a
mtytk <antiklios@gmail.com>
2017-05-30 16:54:29 +0300

    Start

---
+++ src/main/java/com/samples/git/App.java
@@ -0,0 +1,10 @@
+package com.samples.git;
+
+/**
+ * Created by umit on 30/05/2017.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Test...");
+    }
+}
